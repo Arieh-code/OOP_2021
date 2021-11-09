89589ee15a3d29c29c6c1a01ca8448a3b2d7fea3
Arieh Norton <arieh.norton@gmail.com>
2021-11-09 17:44:22 +0200

    Buidling file

---
+++ Assignments/Ex1/src/Building.py
@@ -0,0 +1,4 @@
+import json
+
+class Building:
+    def __init__(self, ):
--- Assignments/Ex1/src/Elevator.py
+++ Assignments/Ex1/src/Elevator.py
@@ -13,4 +13,39 @@ class Elevator:
         self.stoptime = stoptime
 
 
+    def __getPos__(self):
+        pass
+
+    def __goTo__(floor):
+        pass
+
+    def __stop__(floor):
+        pass
+
+    def __getMaxFloor__(self):
+        return self.maxFloor
+
+    def __getMinFloor__(self):
+        return self.minFloor
+
+    def __getId__(self):
+        return self.id
+
+    def __getSpeed__(self):
+        return self.speed
+
+    def __getClosetime__(self):
+        return self.closetime
+
+    def __getOpentime__(self):
+        return self.opentime
+
+    def getstarttime(self):
+        return self.starttime
+
+    def getStoptime(self):
+        return self.stoptime
+
+
+
 
