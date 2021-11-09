6a2cdeb78135773808de4132674e0b7f61849dde
Arieh Norton <arieh.norton@gmail.com>
2021-11-09 17:10:13 +0200

    Elevator class

---
+++ Assignments/Ex1/src/Elevator.py
@@ -0,0 +1,16 @@
+import json
+
+
+class Elevator:
+    def __init__(self, minFloor, maxFloor, id, speed, closetime, opentime, starttime, stoptime):
+        self.minFloor = minFloor
+        self.maxFloor = maxFloor
+        self.id = id
+        self.speed = speed
+        self.closetime = closetime
+        self.opentime = opentime
+        self.starttime = starttime
+        self.stoptime = stoptime
+
+
+
