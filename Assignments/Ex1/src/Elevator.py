3dc1f3893284946862c92d385c770e50b097d7e4
Arieh Norton <arieh.norton@gmail.com>
2021-11-09 20:37:00 +0200

    creating elevator class with functions

--- Assignments/Ex1/src/Elevator.py
+++ Assignments/Ex1/src/Elevator.py
@@ -2,7 +2,7 @@ import json
 
 
 class Elevator:
-    def __init__(self, minFloor, maxFloor, id, speed, closetime, opentime, starttime, stoptime):
+    def __init__(self, id, speed, minFloor, maxFloor, closetime, opentime, starttime, stoptime):
         self.minFloor = minFloor
         self.maxFloor = maxFloor
         self.id = id
@@ -11,41 +11,32 @@ class Elevator:
         self.opentime = opentime
         self.starttime = starttime
         self.stoptime = stoptime
+        self.pos = minFloor  # the elevator is starting at min floor
+        self.state = 0  # the elevator is starting at level state ( 1 is up and -1 is down)
 
+    def goto(self, floor):
+        if self.pos < floor:
+            self.setstate(1)
+        elif self.pos > floor:
+            self.setstate(-1)
+        else:
+            self.setstate(0)
+        self.pos = floor
 
-    def __getPos__(self, floor):
-        pass
+    def setstate(self, num):
+        if self.pos == self.maxFloor:
+            self.state = -1
+        elif self.pos == self.minFloor:
+            self.state = 1
+        else:
+            self.state = num
 
-    def __goTo__(self, floor):
-        pass
+    def stop(self, floor):
+        self.goto(floor)
 
-    def __stop__(self, floor):
-        pass
-
-    def __getMaxFloor__(self):
-        return self.maxFloor
-
-    def __getMinFloor__(self):
-        return self.minFloor
-
-    def __getId__(self):
-        return self.id
-
-    def __getSpeed__(self):
-        return self.speed
-
-    def __getClosetime__(self):
-        return self.closetime
-
-    def __getOpentime__(self):
-        return self.opentime
-
-    def getstarttime(self):
-        return self.starttime
-
-    def getStoptime(self):
-        return self.stoptime
-
+    def __str__(self):
+        return self.id, self.speed, self.minFloor, self.maxFloor, self.closetime, self.opentime, self.starttime, \
+               self.stoptime
 
 
 
