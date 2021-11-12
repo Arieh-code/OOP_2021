d971c94ec247bb112864a40cbcd9282ae730dd50
Arieh Norton <arieh.norton@gmail.com>
2021-11-12 15:03:05 +0200

    updated elevator class

--- Assignments/Ex1/src/Elevator.py
+++ Assignments/Ex1/src/Elevator.py
@@ -3,15 +3,15 @@ import json
 
 class Elevator:
     def __init__(self, id, speed, minFloor, maxFloor, closetime, opentime, starttime, stoptime):
-        self.minFloor = minFloor
-        self.maxFloor = maxFloor
-        self.id = id
-        self.speed = speed
-        self.closetime = closetime
-        self.opentime = opentime
-        self.starttime = starttime
-        self.stoptime = stoptime
-        self.pos = minFloor  # the elevator is starting at min floor
+        self.minFloor = int(minFloor)
+        self.maxFloor = int(maxFloor)
+        self.id = int(id)
+        self.speed = float(speed)
+        self.closetime = float(closetime)
+        self.opentime = float(opentime)
+        self.starttime = float(starttime)
+        self.stoptime = float(stoptime)
+        self.pos = int(minFloor)  # the elevator is starting at min floor
         self.state = 0  # the elevator is starting at level state ( 1 is up and -1 is down)
 
     def goto(self, floor):
@@ -38,5 +38,8 @@ class Elevator:
         return self.id, self.speed, self.minFloor, self.maxFloor, self.closetime, self.opentime, self.starttime, \
                self.stoptime
 
+    def __repr__(self):
+        return self.id, self.speed, self.minFloor, self.maxFloor, self.closetime, self.opentime, self.starttime, \
+               self.stoptime
 
-
+    
