6e967bada109caf8e7e1b6606dd33b699dbd4c25
Arieh Norton <arieh.norton@gmail.com>
2021-11-14 16:02:16 +0200

    added more parameters to elevator

--- Assignments/Ex1/src/Elevator.py
+++ Assignments/Ex1/src/Elevator.py
@@ -13,6 +13,13 @@ class Elevator:
         self.stoptime = float(stoptime)
         self.pos = int(minFloor)  # the elevator is starting at min floor
         self.state = 0  # the elevator is starting at level state ( 1 is up and -1 is down)
+        self.time_stamp = 0.0
+        #########################################################################
+        self.callList = []  # {2}Here we can add the calls appointed to the elevator
+        self.callAmount = 0
+        self.finish_timestamp = 0.0  # {3} The end-time will be initialized to 0.
+        total_floors = (abs(self.minFloor) + abs(self.maxFloor) + 1)
+        self.floor_timestamp_dict = {index: x for index, x in enumerate([0.0] * total_floors, start=self.minFloor)}
 
     def goto(self, floor):
         if self.pos < floor:
@@ -41,5 +48,3 @@ class Elevator:
     def __repr__(self):
         return self.id, self.speed, self.minFloor, self.maxFloor, self.closetime, self.opentime, self.starttime, \
                self.stoptime
-
-    
