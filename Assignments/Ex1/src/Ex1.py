602229eb571e133d6a4578cd05e48d301d6e6431
Arieh Norton <arieh.norton@gmail.com>
2021-11-14 16:02:36 +0200

    The main function for this assignment

---
+++ Assignments/Ex1/src/Ex1.py
@@ -0,0 +1,12 @@
+import json
+from Elevator import *
+from Calls import *
+from Building import *
+
+class Ex1:
+    def __init__(self, json_file, calls_file, out_log):
+        # self.json_file = Building(json_file)
+        # self.calls_file = Calls(calls_file)
+        # self.output_file = output_file
+
+    def
